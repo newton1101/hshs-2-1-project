8dd058caad9be394b9d15a957f18f516babe87cc
jschrislee <37567625+jschrislee@users.noreply.github.com>
2019-08-20 11:50:58 +0900

    Update 누군가의 연구소(1학기).py
    사용자가 쓸 벡터 플라스미드의 염기서열을 입력할수 있도록 기능을 추가했다.

--- 누군가의 연구소(1학기).py
+++ 누군가의 연구소(1학기).py
@@ -6,31 +6,35 @@
 
 EcoRI = 'TTAACCTATAAAAATAGGCGTATCACGAGGCAGAATTTCAGATAAAAAAAATCCTTAGCTTTCGCTAAGG' +'ATGATTTCTGGAATTCGCGGCCGCTTCTAGAGTTGACGGCTAGCTCAGTCCTAGGTACAGTGCTAGCTAC' +'TAGAGAAAGAGGAGAAATACTAGATGGCCGGCGCACGCAACGCGACCAATAAGCTCCTGCACAAAGCGAA' +'AAAAAGCAAAAGCGATGAGTTTTATACCCAGTATTGCGACATTGAAAACGAATTACAATACTACCGCGAA' +'CATTTCAGCGATAAGGTTGTGTACTGCAACTGTGATGACCCGCGTGTGAGCAATTTTTTTAAATATTTCG' +'CGGTGAACTTTGACAACCTCGGACTGAAGAAGCTGATTGCAAGCTGTTATGTAGAAAACAAGGAGGGCTT' +'TAGCTCCAGCGAAGCAGCAAAAAATGGCTTTTACTATGAATACCATAAAGAAAACGGCAAAAAACTGGTC' +'TTCGATGATATTTCGGTGAGTAGCTTCTGTGGCGACGGAGATTTTCGGAGCAGTGAAAGCATTGATCTGC' +'TCAAAAAAAGTGATATCGTGGTCACGAACCCGCCTTTTTCACTCTTTCGTGAATACTTAGATCAACTTAT' +'TAAATACGATAAAAAGTTCCTTATCATCGCCAATGTAAACAGCATTACTTATAAAGAAGTGTTTAATCTG' +'ATCAAAGAAAACAAGATCTGGCTGGGAGTACATTTAGGCCGGGGTGTGAGTGGCTTCATCGTTCCAGAAC' +'ATTACGAACTGTACGGCACCGAGGCCCGCATTGATTCAAATGGAAACCGTATTATCAGCCCGAATAACTG' +'TCTGTGGCTGACGAACCTTGACGTCTTCATTCGCCACAAAGACCTGCCGTTAACACGTAAATACTTTGGA' +'AACGAAAGCTCGTAC'
 
-def comp(EcoRI):  #상보적 염기서열
+def comp(vector):  #상보적 염기서열
     comp_dict={'A':'T', 'T':'A', 'C':'G', 'G':'C'}
-    EcoRI_comp=""
-    for char in EcoRI:
+    vector_comp=""
+    for char in vector:
         if char in comp_dict:
-            EcoRI_comp = EcoRI_comp + comp_dict[char]
-    return EcoRI_comp
+            vector_comp = vector_comp + comp_dict[char]
+    return vector_comp
  
-def rev(EcoRI): #역순 염기서열
+def rev(vector): #역순 염기서열
     comp_dict={'A':'A', 'T':'T', 'C':'C', 'G':'G'}
-    rev_str = "".join(reversed(EcoRI))
-    EcoRI_rev = ""
+    rev_str = "".join(reversed(vector))
+    vector_rev = ""
     for char in rev_str:
         if char in comp_dict:
-            EcoRI_rev = EcoRI_rev + comp_dict[char]
-    return EcoRI_rev
+            vector_rev = vector_rev + comp_dict[char]
+    return vector_rev
 
-def cke(EcoRI, pattern): #제한효소자리 검사
+def cke(vector, pattern): #제한효소자리 검사
     pattern_list = []
-    for i in range(0, len(EcoRI) - len(pattern)) :
-        if EcoRI[i:i+len(pattern)] == pattern : 
-            pattern_list.append((i+1, EcoRI[i:i+len(pattern)]))
+    for i in range(0, len(vector) - len(pattern)) :
+        if vector[i:i+len(pattern)] == pattern : 
+            pattern_list.append((i+1, vector[i:i+len(pattern)]))
+    for i in range(0, len(vector) - len(pattern)) :
+        if vector[i:i+len(pattern)] == pattern : 
+            pattern_list.append((i+1, vector[i:i+len(pattern)]))
     return pattern_list
 
+vector = str(input('enter sequence of your vector').upper())
 pattern = comp(input('사용할 제한효소의 염기서열을입력해주세요.').upper())
-i = cke(EcoRI, pattern)
+i = cke(vector, pattern)
 print(i)
 
