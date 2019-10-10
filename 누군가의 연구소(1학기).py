7304c5efb446d4aef7669134a15bb1f342a0083e
cjswoqkqh9612 <40245435+cjswoqkqh9612@users.noreply.github.com>
2019-10-10 09:30:52 +0900

    결과를 더 알아보기 쉽게 변경

--- 누군가의 연구소(1학기).py
+++ 누군가의 연구소(1학기).py
@@ -1,8 +1,6 @@
-
 # coding: utf-8
 
 # In[3]:
-
 
 EcoRI = 'TTAACCTATAAAAATAGGCGTATCACGAGGCAGAATTTCAGATAAAAAAAATCCTTAGCTTTCGCTAAGG' +'ATGATTTCTGGAATTCGCGGCCGCTTCTAGAGTTGACGGCTAGCTCAGTCCTAGGTACAGTGCTAGCTAC' +'TAGAGAAAGAGGAGAAATACTAGATGGCCGGCGCACGCAACGCGACCAATAAGCTCCTGCACAAAGCGAA' +'AAAAAGCAAAAGCGATGAGTTTTATACCCAGTATTGCGACATTGAAAACGAATTACAATACTACCGCGAA' +'CATTTCAGCGATAAGGTTGTGTACTGCAACTGTGATGACCCGCGTGTGAGCAATTTTTTTAAATATTTCG' +'CGGTGAACTTTGACAACCTCGGACTGAAGAAGCTGATTGCAAGCTGTTATGTAGAAAACAAGGAGGGCTT' +'TAGCTCCAGCGAAGCAGCAAAAAATGGCTTTTACTATGAATACCATAAAGAAAACGGCAAAAAACTGGTC' +'TTCGATGATATTTCGGTGAGTAGCTTCTGTGGCGACGGAGATTTTCGGAGCAGTGAAAGCATTGATCTGC' +'TCAAAAAAAGTGATATCGTGGTCACGAACCCGCCTTTTTCACTCTTTCGTGAATACTTAGATCAACTTAT' +'TAAATACGATAAAAAGTTCCTTATCATCGCCAATGTAAACAGCATTACTTATAAAGAAGTGTTTAATCTG' +'ATCAAAGAAAACAAGATCTGGCTGGGAGTACATTTAGGCCGGGGTGTGAGTGGCTTCATCGTTCCAGAAC' +'ATTACGAACTGTACGGCACCGAGGCCCGCATTGATTCAAATGGAAACCGTATTATCAGCCCGAATAACTG' +'TCTGTGGCTGACGAACCTTGACGTCTTCATTCGCCACAAAGACCTGCCGTTAACACGTAAATACTTTGGA' +'AACGAAAGCTCGTAC'
 
@@ -30,7 +28,28 @@ def cke(EcoRI, pattern): #제한효소자리 검사
             pattern_list.append((i+1, EcoRI[i:i+len(pattern)]))
     return pattern_list
 
-pattern = comp(input('사용할 제한효소의 염기서열을입력해주세요.').upper())
-i = cke(EcoRI, pattern)
-print(i)
+def Limit_enzyme(EcoRI): #제한효소자리 길이를 구하기 위한 리스트 제작
+    limit_enzyme_list = []
+    for i in range(0, len(EcoRI) - len(pattern)) :
+        if EcoRI[i:i+len(pattern)] == pattern : 
+            limit_enzyme_list.append((i+1))
+            
+    base_pair_list = []
+    for i in range (0, len(pattern)-2) :
+        if len(limit_enzyme_list) == 0 or 1:
+            base_pair_list.append(len(EcoRI))
+        else :
+            base_pair_list.append(limit_enzyme_list[i+1]-limit_enzyme_list[i]) #제한효소 부착자리 개수에 따른 절편의 길이 구하기
+            base_pair_list.append(len(EcoRI) - (limit_enzyme_list[-1] - limit_enzyme_list[0])) #제한효소 절편들중 세어지지 않은 절편 1개 셈
+    return base_pair_list
 
+pattern = (input('사용할 제한효소의 염기서열을입력해주세요.').upper())
+
+y = cke(EcoRI, pattern)
+z = Limit_enzyme(EcoRI)
+for s in range(len(y)):    
+    print(y[s][0],'번째 염기서열에',y[s][1],'의 제한효소 부착자리가 존재')
+    print(str(z)[s][0],'bp')
+if len(y) == 0:
+    print('그런거없다')
+    
